145377f46bf881df00745eda649209480178f5cb
Maria Z <z0559882272@gmail.com>
2025-02-19 18:49:36 +0200

    added registration tests

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,8 +1,13 @@
 package manager;
 
+import models.User;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class HelperUser extends HelperBase{
 
@@ -35,6 +40,11 @@ public void openLoginRegistrationForm(){
         type(By.xpath("//input[last()]"),password);
     }
 
+    public void fillLoginRegistrationForm(User user) {
+        type(By.name("email"), user.getEmail());
+        type(By.xpath("//input[last()]"), user.getPassword());
+    }
+
 
     public void sumitLogin(){
         click(By.xpath("//button[text()='Login']"));
@@ -46,5 +56,16 @@ public void openLoginRegistrationForm(){
 
     public void logout() {
         click(By.xpath("//button[text() = 'Sign Out']"));
+    }
+
+    public void submitRegistration() {
+        click(By.xpath("//button[text()='Registration']"));
+    }
+
+    public boolean isNoContactsHereDisplayed() {
+        WebDriverWait wait = new WebDriverWait(wd, Duration.ofSeconds(5));
+       return wait.until(ExpectedConditions
+                .textToBePresentInElement(wd.findElement(By.cssSelector(".contact-page_message__2qafk>h1")),
+                        "No Contacts here!"));
     }
 }
