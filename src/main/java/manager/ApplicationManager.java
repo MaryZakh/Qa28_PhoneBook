099be220535685cd5c6298f9c0ea88ed0b03473f
Maria Z <z0559882272@gmail.com>
2025-03-02 19:27:53 +0200

    added ListenersTestNG && WDListeners

--- src/main/java/manager/ApplicationManager.java
+++ src/main/java/manager/ApplicationManager.java
@@ -33,7 +33,6 @@ public class ApplicationManager {
         helperContact = new HelperContact(wd);
 
 
-
     }
 
 
--- src/main/java/manager/WDListener.java
+++ src/main/java/manager/WDListener.java
@@ -1,14 +1,18 @@
 package manager;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+import com.google.common.io.Files;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.events.WebDriverListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.List;
+import java.util.Random;
 
 public class WDListener implements WebDriverListener {
 
@@ -36,7 +40,7 @@ public class WDListener implements WebDriverListener {
     @Override
     public void afterClick(WebElement element) {
         WebDriverListener.super.afterClick(element);
-        logger.info("After click element-->" + element.getTagName() + " text = " + element.getText());
+        logger.info("After click element-->" + element.getLocation() + " text = " + element.getText());
 
     }
 
@@ -47,5 +51,40 @@ public class WDListener implements WebDriverListener {
         logger.info("sendKys-->" +element.getTagName() );
     }
 
+    @Override
+    public void beforeTo(WebDriver.Navigation navigation, String url) {
+        WebDriverListener.super.beforeTo(navigation, url);
+        logger.info("url -->" + url);
+    }
 
+    @Override
+    public void beforeClear(WebElement element) {
+        WebDriverListener.super.beforeClear(element);
+        logger.info("clear element -->" + element);
+    }
+
+
+    @Override
+    public void onError(Object target, Method method, Object[] args, InvocationTargetException e) {
+        WebDriverListener.super.onError(target, method, args, e);
+        logger.info("Huston we have a problem!");
+        logger.info("Object target--->" + target.toString());
+        logger.info("*******************");
+        logger.info("Method name--->" + method.getName());
+        logger.info("*******************");
+        logger.info("InvocationTargetException" + e.getTargetException());
+        logger.info("*******************");
+
+        int i  = new Random().nextInt(1000)+1000;
+        String link = "src/test/screenshots/screen_"+ i +".png";
+        logger.info("Screen with error is--->" + link);
+        WebDriver wd = (WebDriver) target;
+        TakesScreenshot takesScreenshot = (TakesScreenshot) wd;
+        File tmp = takesScreenshot.getScreenshotAs(OutputType.FILE);
+        try {
+            Files.copy(tmp,new File(link));
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
 }
