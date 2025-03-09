7b1dcb6b12e5cc6b9dc5a7c0361212dd5e7eeb63
Maria Z <z0559882272@gmail.com>
2025-03-09 20:03:36 +0200

    added DataProvider from File

--- src/test/java/tests/AddNewContactTests.java
+++ src/test/java/tests/AddNewContactTests.java
@@ -1,5 +1,6 @@
 package tests;
 
+import manager.DataProviderContact;
 import models.Contact;
 import models.User;
 import org.testng.Assert;
@@ -18,22 +19,36 @@ public class AddNewContactTests extends TestBase {
     }
 
 
-    @Test
-    public void addContactSuccessAllFields() {
+    @Test(dataProvider = "contactSuccess", dataProviderClass = DataProviderContact.class)
+    public void addContactSuccessAllFields(Contact contact) {
         int i = (int) ((System.currentTimeMillis() / 1000) % 3600);
-
-        Contact contact = Contact.builder()
-                .name("Tony" + i)
-                .lastName("Stark")
-                .address("NY")
-                .phone("34343434" + i)
-                .email("stark" + i + "@gmail.com")
-                .description("all fields")
-                .build();
+//
+//        Contact contact = Contact.builder()
+//                .name("Tony" + i)
+//                .lastName("Stark")
+//                .address("NY")
+//                .phone("34343434" + i)
+//                .email("stark" + i + "@gmail.com")
+//                .description("all fields")
+//                .build();
         logger.info("Tests run with data: --->" + contact.toString());
         app.getHelperContact().openContactForm();
         app.getHelperContact().fillContactForm(contact);
        // app.getHelperContact().pause(15000);
+        app.getHelperContact().getScreen("src/test/screenshots/screen-" + i +".png");
+        app.getHelperContact().saveContact();
+        Assert.assertTrue(app.getHelperContact().isContactAddedByName(contact.getName()));
+        Assert.assertTrue(app.getHelperContact().isContactAddedByPhone(contact.getPhone()));
+    }
+
+    @Test(dataProvider = "contactCSV", dataProviderClass = DataProviderContact.class)
+    public void addContactSuccessAllFieldsCSV(Contact contact) {
+        int i = (int) ((System.currentTimeMillis() / 1000) % 3600);
+
+        logger.info("Tests run with data: --->" + contact.toString());
+        app.getHelperContact().openContactForm();
+        app.getHelperContact().fillContactForm(contact);
+        // app.getHelperContact().pause(15000);
         app.getHelperContact().getScreen("src/test/screenshots/screen-" + i +".png");
         app.getHelperContact().saveContact();
         Assert.assertTrue(app.getHelperContact().isContactAddedByName(contact.getName()));
@@ -113,16 +128,16 @@ public class AddNewContactTests extends TestBase {
         Assert.assertTrue(app.getHelperContact().isAddPageStillDisplayed());
     }
 
-    @Test
-    public void addNewContactWrongPhone() {
-        Contact contact = Contact.builder()
-                .name("Tony")
-                .lastName("Stark")
-                .address("NY")
-                .phone("")
-                .email("stark@gmail.com")
-                .description("Empty Phone")
-                .build();
+    @Test(dataProvider = "ContactWrongPhone",dataProviderClass = DataProviderContact.class)
+    public void addNewContactWrongPhone(Contact contact) {
+//        Contact contact = Contact.builder()
+//                .name("Tony")
+//                .lastName("Stark")
+//                .address("NY")
+//                .phone("")
+//                .email("stark@gmail.com")
+//                .description("Empty Phone")
+//                .build();
         logger.info("Tests run with data: --->" + contact.toString());
         app.getHelperContact().openContactForm();
         app.getHelperContact().fillContactForm(contact);
